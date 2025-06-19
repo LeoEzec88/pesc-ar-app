e747214f98f145539f778134e03130ba222d5032
Leonardo Coronel <lcoronel.unab@gmail.com>
2025-06-19 18:48:26 -0300

    cambios en index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {connectDB} from './db.js';
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
